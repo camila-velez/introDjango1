d12a78e8f2abecfe58477154fa47dddb08e3f122
Camila Velez <cvelezg13@eafit.edu.co>
2025-07-28 12:44:05 -0500

    Primer commit

---
+++ Arqui/pages/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        return context
+
